95aaf6808fe55aa258b109fb236604a6555cadeb
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2021-06-02 11:38:38 -0400

    fix build error
    Can't use babel on mapbox-gl: https://docs.mapbox.com/mapbox-gl-js/api/#transpiling-v2

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import mapboxgl from "mapbox-gl";
+import mapboxgl from "!mapbox-gl";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
